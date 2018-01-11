e967cea02dc37e4ded96fc4a1fa963577e09c51d
Sophie Ciulla <Sophie.Ciulla@MU-C1-09.mullen.pvt.k12.co.us>
2018-01-11 14:33:24 -0700

    initial

--- Modulus Quizzer/src/ModulusQuizzer.java
+++ Modulus Quizzer/src/ModulusQuizzer.java
@@ -1,42 +1,77 @@
 import java.util.Scanner;
 public class ModulusQuizzer
 	{
+		static int answer;
+		static int randomNumberOne;
+		static int randomNumberTwo;
+		static int modAnswer;
+		static int numberRight;
+		static int questions;
+		//hello
 
 		public static void main(String[] args)
 			{
 				askQuestions();
+				askWeirdQuestions();
+//				checkAnswer();
+//				tellNumberRight();
 			}
 		public static void askQuestions()
 		{
 			Scanner userInput = new Scanner(System.in);
-			System.out.println("Hello, how many questions would you like to have?");
-			int questions = userInput.nextInt();
-			int numberRight = 0;
+			System.out.println("Hello, how many questions would you like to have? Please choose at least 4");
+			questions = userInput.nextInt();
+			numberRight = 0;
 			
-			for (int i =0; i<questions; i++)
+			for (int i =0; i<questions-2; i++)
 				{
-					int randomNumberOne = (int)(Math.random()*10)+1;
-					int randomNumberTwo = (int)(Math.random()*15)+5;
-					int modAnswer = randomNumberOne%randomNumberTwo;
+					randomNumberOne = (int)(Math.random()*16)+1;
+					randomNumberTwo = (int)(Math.random()*8)+1;
+					modAnswer = randomNumberOne%randomNumberTwo;
+					if(randomNumberOne<=randomNumberTwo)
+						{
+							randomNumberOne = randomNumberOne+9;
+						}
 					System.out.println("What is " + randomNumberOne + " % " +  randomNumberTwo);
-					
-					int answer = userInput.nextInt();
+					answer = userInput.nextInt();
+					checkAnswer();
+				}
+			
+		}	
+		public static void askWeirdQuestions()
+		{
+			Scanner userInput = new Scanner(System.in);
+			System.out.println("What is " + randomNumberOne + " % " + randomNumberOne);
+			modAnswer = randomNumberOne%randomNumberOne;
+			answer = userInput.nextInt();
+			checkAnswer();
+			randomNumberTwo = randomNumberTwo + 20;
+			System.out.println("What is " + randomNumberOne + " % " + randomNumberTwo);
+			answer = userInput.nextInt();
+			modAnswer = randomNumberOne%randomNumberTwo;
+			checkAnswer();
+			tellNumberRight();
+		}
+		public static void checkAnswer()
+		{
 					if(answer == modAnswer)
 						{
 							numberRight++;
 							System.out.println("That is correct");
-							System.out.println("What is " + randomNumberOne + " % " + randomNumberOne);
 						}
 					else
 						{
 							System.out.println("That is incorrect");
 						}
-					if(randomNumberOne < randomNumberTwo)
-						{
-							questions++;
-						}
-				}
+					
+		}
+		public static void tellNumberRight()
+		{
 			System.out.println("You got " + numberRight + " out of " + questions + " questions right");
 		}
+			
+			
+		
+		
 
 	}
